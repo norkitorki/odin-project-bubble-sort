c6798bd8c6bdd92927b82ccd546cbb5d3928688c
norkitorki <norkitorki@tutanota.de>
2021-02-08 09:33:20 +0100

    refactoring bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,14 @@
 def bubble_sort(arr)
-  check = 0
-  while (check < arr.length - 1)
-    check += 1 if arr[check] < arr[check + 1]
-    left = 0
-    left += 1 while (arr[left + 1] && arr[left] < arr[left + 1])
+  progress = 0
+  while (progress < arr.length - 1)
+    i = 0
+    i += 1 while(arr[i + 1] && arr[i] < arr[i + 1])
 
-    right = left + 1
-    while(arr[right] && arr[left] > arr[right])
-      arr[left], arr[right] = arr[right], arr[left]
-      left += 1
-      right += 1
+    while (arr[i + 1] && arr[i] > arr[i + 1])
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i += 1
     end
+    progress += 1 if arr[progress] < arr[progress + 1]
   end
   arr
 end
